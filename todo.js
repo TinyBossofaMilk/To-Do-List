8c9af2c44720e5a7b67b92885e870d2eba94de84
TinyBossofaMilk <ppcctan@gmail.com>
2022-06-28 23:47:39 -0700

    first commit

---
+++ todo.js
@@ -0,0 +1,27 @@
+//todo item factory function
+let Item = () => {
+    return {title, description, creationDate, dueDate, isCreated, parentProject};    
+    // return {title, description, dueDate, creationDate, parentProject}
+};
+
+let display = (() => {
+
+    let _projects = {
+        defaultProject: []
+    }
+
+    const _initialize = () => {
+        document.getElementById('addItem');
+
+        //getproject
+    };
+
+
+
+
+
+
+
+
+
+}) ();
