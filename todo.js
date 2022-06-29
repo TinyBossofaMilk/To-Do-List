a49a098bb6451277092685f9010ee11d60ea6265
TinyBossofaMilk <ppcctan@gmail.com>
2022-06-29 02:04:39 -0700

    more progress on running module

--- todo.js
+++ todo.js
@@ -1,49 +1,60 @@
 //todo item factory function
-let Item = () => {
-    return {title, description, isCompleted, creationDate, dueDate};    
+let Item = (title, description, isCompleted) => {
+    return {title, description, isCompleted}; //, creationDate, dueDate};    
     // return {title, description, dueDate, creationDate, parentProject}
 };
 
 let display = (() => {
 
     let _projects = {
-        defaultProject: [Item('sampleTitle', 'sampledescription', )]
+        defaultProject: [Item('sampleTitle', 'sampledescription', false)]
     };
 
-    let _activeProject = 0;
+    let _activeProject = 'defaultProject';
 
     const _initialize = () => {
-        document.getElementById('addItem');
-
-        //getproject
+        //give addToDoItem button function
+        const addItem = document.getElementById('addItem');
+        
+        //populate to do table
+        const display = document.getElementById('display');
+        //clear children
+        //get active
+        _populateList();
+        
     };
 
-    
+
+    const _changeItemCompletionStatus = (e) => {
+
+    };
+
     //helper function for populating display
-    const _createItemElement = () => {
+    const _createItemElement = (item) => {
         const tempDiv = document.createElement('div');
         tempDiv.classList.add('item');
-        const input = document.createElement('input');
-        input.addEventListener('toggle', )
 
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        input.addEventListener('toggle', _changeItemCompletionStatus);
+
+        const title = document.createElement('div');
+        title.innerHTML = item.title;
+        
+        tempDiv.appendChild(input);
+        tempDiv.appendChild(title);
         return tempDiv;
     };
 
     const _populateList = () => {
         const display = document.getElementById('display');
-
-        for(const item of _projects[defaultProject])
+        
+        for(const item of _projects[_activeProject])
         {
-            
-            
-
+            display.appendChild(_createItemElement(item));
         }
     };
 
 
-
-
-
-
-
+    _initialize();
 }) ();
