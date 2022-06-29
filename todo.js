5c0b01d5f35728cd801bdd14a4811b162369ab56
TinyBossofaMilk <ppcctan@gmail.com>
2022-06-29 01:09:26 -0700

    working on js

--- todo.js
+++ todo.js
@@ -1,14 +1,16 @@
 //todo item factory function
 let Item = () => {
-    return {title, description, creationDate, dueDate, isCreated, parentProject};    
+    return {title, description, isCompleted, creationDate, dueDate};    
     // return {title, description, dueDate, creationDate, parentProject}
 };
 
 let display = (() => {
 
     let _projects = {
-        defaultProject: []
-    }
+        defaultProject: [Item('sampleTitle', 'sampledescription', )]
+    };
+
+    let _activeProject = 0;
 
     const _initialize = () => {
         document.getElementById('addItem');
@@ -16,7 +18,27 @@ let display = (() => {
         //getproject
     };
 
+    
+    //helper function for populating display
+    const _createItemElement = () => {
+        const tempDiv = document.createElement('div');
+        tempDiv.classList.add('item');
+        const input = document.createElement('input');
+        input.addEventListener('toggle', )
 
+        return tempDiv;
+    };
+
+    const _populateList = () => {
+        const display = document.getElementById('display');
+
+        for(const item of _projects[defaultProject])
+        {
+            
+            
+
+        }
+    };
 
 
 
