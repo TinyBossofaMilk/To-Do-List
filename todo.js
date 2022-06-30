507c09d2a936684ba5812ac45f98698c1e36a086
TinyBossofaMilk <ppcctan@gmail.com>
2022-06-29 18:29:10 -0700

    added projects + display functions

--- todo.js
+++ todo.js
@@ -1,57 +1,86 @@
-//todo item factory function
-let Item = (title, description, isCompleted) => {
+//todo goal factory function
+let goal = (title, description, isCompleted) => {
     return {title, description, isCompleted}; //, creationDate, dueDate};    
     // return {title, description, dueDate, creationDate, parentProject}
 };
 
 let display = (() => {
-
     let _projects = {
-        defaultProject: [Item('sampleTitle', 'sampledescription', false)]
+        defaultProject: [goal('sampleTitle', 'sampledescription', false),
+                         goal('another note', 'sampledescription', true)],
+
+        'get a job' : [goal('complete the Odin Project', 'sampledescription', false),
+                      goal('make a capstone project', 'sampledescription', false),
+                      goal('apply for 50 jobs', 'sampledescription', false),
+                      goal('finish "Cracking the Coding interview"', 'sampledescription', false),]
     };
 
     let _activeProject = 'defaultProject';
 
     const _initialize = () => {
-        //give addToDoItem button function
-        const addItem = document.getElementById('addItem');
+        //give addToDogoal button function
+        const addgoal = document.getElementById('addgoal');
         
         //populate to do table
         const display = document.getElementById('display');
         //clear children
         //get active
-        _populateList();
-        
+        _populateProjectsList();
+        _populateGoalsList();
     };
 
+    const _populateProjectsList = () => {
+        const projectsTab = document.getElementById('projects');
 
-    const _changeItemCompletionStatus = (e) => {
+        //iterate through and create a goal element
+        Object.keys(_projects).forEach(name => {
+            const project = document.createElement('div');
+            project.innerHTML = name;
+            project.addEventListener('click', () =>
+            {
+                _activeProject = project.innerHTML;
+                _populateGoalsList();
+            });
 
+            projectsTab.appendChild(project);            
+        });
+    };
+
+    const _changegoalCompletionStatus = (e, goal) => {
+        goal.isCompleted = e.checked;
+        console.log("_projects")
+        console.log(goal)
     };
 
     //helper function for populating display
-    const _createItemElement = (item) => {
+    const _creategoalElement = (goal) => {
         const tempDiv = document.createElement('div');
-        tempDiv.classList.add('item');
+        tempDiv.classList.add('goal');
 
         const input = document.createElement('input');
         input.type = 'checkbox';
-        input.addEventListener('toggle', _changeItemCompletionStatus);
+        input.checked = goal.isCompleted;
+        input.addEventListener('toggle', _changegoalCompletionStatus(input, goal));
 
-        const title = document.createElement('div');
-        title.innerHTML = item.title;
+        const title = document.createElement('span');
+        title.innerHTML = goal.title;
         
         tempDiv.appendChild(input);
         tempDiv.appendChild(title);
         return tempDiv;
     };
 
-    const _populateList = () => {
+    const _populateGoalsList = () => {
         const display = document.getElementById('display');
         
-        for(const item of _projects[_activeProject])
+        while(display.hasChildNodes())
         {
-            display.appendChild(_createItemElement(item));
+            display.removeChild(display.firstChild);
+        }
+
+        for(const goal of _projects[_activeProject])
+        {
+            display.appendChild(_creategoalElement(goal));
         }
     };
 
